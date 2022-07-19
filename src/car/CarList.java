77bbd91e37ec55927e503d64d85c07722561d436
TLupus131 <109510927+TLupus131@users.noreply.github.com>
2022-07-19 14:45:13 +0700

    Update CarList.java

--- src/car/CarList.java
+++ src/car/CarList.java
@@ -22,7 +22,7 @@ public class CarList extends ArrayList<Car> {
         this.brandList = brandList;
     }
 
-public boolean loadFromFile(String filename) {
+    public boolean loadFromFile(String filename) {
         try {
             File f = new File(filename);
             if (!f.exists()) {
@@ -33,7 +33,7 @@ public boolean loadFromFile(String filename) {
             BufferedReader br = new BufferedReader(fr);
             String line;
             while ((line = br.readLine()) != null) {
-                StringTokenizer stk = new StringTokenizer(line, ",");
+                StringTokenizer stk = new StringTokenizer(line, ", ");
                 String carID = stk.nextElement().toString();
                 String brandID = stk.nextElement().toString();
                 int pos = brandList.searchID(brandID);
@@ -51,16 +51,29 @@ public boolean loadFromFile(String filename) {
             return false;
         }
     }
-
+    
     public void listCars() {
-
-//        Collections.sort(this.);
+        int p1, p2;
+        
+        for (int i = 0; i < this.size(); i++) {
+            for (int j = i + 1; j < this.size(); j++) {
+                Car c1 = (Car) this.get(j);
+                Car c2 = (Car) this.get(i);
+                
+                p1 = this.indexOf(c1);
+                p2 = this.indexOf(c2);
+                
+                if (c1.comparedTo(c2) < 0) {
+                    Collections.swap(this, p1, p2);
+                }
+            }
+        }
         for (Car car : this) {
-            System.out.println(car);
+            System.out.println(car.screenString());
         }
         System.out.println("\n\n");
     }
-
+    
     public int searchID(String carID) {
         for (int i = 0; i < this.size(); i++) {
             if (carID.equals(this.get(i).carID)) {
@@ -69,7 +82,7 @@ public boolean loadFromFile(String filename) {
         }
         return -1;
     }
-
+    
     public int searchFrame(String fID) {
         for (int i = 0; i < this.size(); i++) {
             if (fID.equals(this.get(i).frameID)) {
@@ -78,7 +91,7 @@ public boolean loadFromFile(String filename) {
         }
         return -1;
     }
-
+    
     public int searchEngine(String eID) {
         for (int i = 0; i < this.size(); i++) {
             if (eID.equals(this.get(i).engineID)) {
@@ -87,10 +100,8 @@ public boolean loadFromFile(String filename) {
         }
         return -1;
     }
-
+    
     public void addCar() {
-        CarList carlist = new CarList(brandList);
-        carlist.loadFromFile("cars.txt");
         Scanner sc = new Scanner(System.in);
         String carID = null;
         String brandID = null;
@@ -146,7 +157,6 @@ public boolean loadFromFile(String filename) {
                 }
             }
         }
-
         while (true) {
             System.out.print("Enter engine ID: ");
             engineID = sc.nextLine();
@@ -166,10 +176,149 @@ public boolean loadFromFile(String filename) {
                 }
             }
         }
-//        Brand t = new Brand(ID, brandName, soundBrand, price);
+//        Car t = new Car(carID, brand, colour, frameID, engineID);
 //        this.add(t);
-//        for (Brand brand : this) {
-//            System.out.println(brand);
-//        }
+    }
+    
+    public boolean saveToFile(String filename) {
+        try {
+            File f = new File(filename);
+            boolean writeMode = OVERWRITE;
+            FileWriter fw = new FileWriter(f, writeMode);
+            PrintWriter pw = new PrintWriter(fw);
+            for (Car car : this) {
+                pw.println(car);
+            }
+            pw.close();
+            fw.close();
+            System.out.println("Add car to " + filename + " success.");
+            return true;
+        } catch (Exception e) {
+            System.err.println("Cannot write to file " + filename + "!");
+            System.err.println("Error reason: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean removeCar() {
+        Scanner sc = new Scanner(System.in);
+        String removedID = null;
+        System.out.print("Enter car ID: ");
+        removedID = sc.nextLine();
+        if (searchID(removedID) == -1) {
+            System.out.println("Not found!!!");
+            return false;
+        }
+        int pos = searchID(removedID);
+        this.remove(pos);
+        System.out.println("Remove successfully.");
+        return true;
+    }
+    
+    public void updateCar() {
+        Scanner sc = new Scanner(System.in);
+        String carID = null;
+        String newCarID = null;
+        String newBrandID = null;
+        String newColour = null;
+        String newFrameID = null;
+        String newEngineID = null;
+        System.out.print("Enter car ID: ");
+        carID = sc.nextLine();
+        if (searchID(carID) == -1) {
+            System.out.println("Not found!");
+        } else {
+            Car CarUpdate = this.get(searchID(carID));
+            while (true) {
+                System.out.print("Enter new ID: ");
+                newCarID = sc.nextLine();
+                if (searchID(newCarID) == -1) {
+                    break;
+                }
+                System.out.println("ID already exist, please enter again !!!");
+            }
+// đoạn này chỉ cần lắp menu.e ref_getChoice là xong
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+            while (true) {
+                System.out.print("Enter new colour: ");
+                newColour = sc.nextLine();
+                String c = newColour.trim();
+                if (!c.isEmpty()) {
+                    break;
+                } else {
+                    System.out.println("Colour can't be blank, please enter again!!!");
+                }
+            }
+            while (true) {
+                System.out.print("Enter new frame ID: ");
+                newFrameID = sc.nextLine();
+                char c = newFrameID.charAt(0);
+                int count = 0;
+                for (int i = 0; i < newFrameID.length(); i++) {
+                    char c2 = newFrameID.charAt(i);
+                    if ((c2 < 48) || (c2 > 57)) {
+                        count++;
+                    }
+                }
+                if ((c == 'F') && (count == 0) && (newFrameID.length() == 5)) {
+                    if (searchFrame(newFrameID) == -1) {
+                        break;
+                    } else {
+                        System.out.println("Frame ID already exist, please enter again !!!");
+                    }
+                }
+            }
+            while (true) {
+                System.out.print("Enter new engine ID: ");
+                newEngineID = sc.nextLine();
+                char c = newEngineID.charAt(0);
+                int count = 0;
+                for (int i = 0; i < newEngineID.length(); i++) {
+                    char c2 = newEngineID.charAt(i);
+                    if ((c2 < 48) || (c2 > 57)) {
+                        count++;
+                    }
+                }
+                if ((c == 'E') && (count == 0) && (newEngineID.length() == 5)) {
+                    if (searchEngine(newEngineID) == -1) {
+                        break;
+                    } else {
+                        System.out.println("Frame ID already exist, please enter again !!!");
+                    }
+                }
+            }
+            CarUpdate.setCarID(newCarID);
+//            CarUpdate.setBrand(newBrandID);
+            CarUpdate.setColor(newColour);
+            CarUpdate.setFrameID(newFrameID);
+            CarUpdate.setEngineID(newEngineID);
+        }
+    }
+    
+    public void printBasedBrandName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String aPartOfBrandName = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < this.size(); i++) {
+            Car c = this.get(i);
+            if (c.brand.brandName.contains(aPartOfBrandName)) {
+                System.out.println(c.screenString());
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("No car is detected!");
+        }
     }
 }
